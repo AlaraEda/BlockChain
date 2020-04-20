6693a9e09da2a6d8fb3b0930d2f059ac893b89be
AlaraEdda <0931871@hr.nl>
2020-04-20 20:09:27 +0200

    Add

--- index.js
+++ index.js
@@ -1,22 +1,57 @@
-let string = "text";
 
+//My Code
+let string = "tex";
+var theCode ="";
+let output = []
+
+//Loop door je string
 for (let i = 0; i < string.length; i++) { 
     //Text omzetten naar ASC11- Waardes
-    let theCode = string.charCodeAt(i);  
-    
-    //Getallen scheiden met een komma
-    let output = [],
-    sCodes = theCode.toString();
-
-    for (let o = 0, len = sCodes.length; o < len; o += 1) {
-        output.push(+sCodes.charAt(o));
-        
-    }
-    
-    console.log(output);
+    let ASC11 = string.charCodeAt(i);
+    theCode = theCode + ASC11                 //Zorg ervoor dat cijfers op een rij staan.
 }
 
-//console.log(String.fromCodePoint(101))              //ASC11-Waardes omzetten naar text.
+//Zet elke getal los/gescheiden in een array.
+let sCodes = theCode.toString();
+for (let o = 0; o < sCodes.length; o ++) {
+    output.push(+sCodes.charAt(o));           
+}
 
-let char = String.fromCharCode(688)
-console.log(char)
+//Check hoe groot de array is
+if (output.length == 10){
+    console.log(output);
+    console.log("Lengte van de output:", output.length)
+    console.log("Reeks is een veelvoud van 10")
+}
+
+if (output.length < 10){
+    console.log(output);
+    console.log("Lengte van de output:", output.length)
+    console.log("Vul reeks aan tot het bestaat uit 10 gehele getallen.")
+
+    //Vul reeks aan met getallen.
+    //Stop met aanvullen als output.length == 10
+    //Vul array aan met output.push(+sCodes.charAt(o));
+
+
+    let extraCijfers = '';
+    for (let aanvullen = 0; aanvullen < 10; aanvullen++){
+        // extraCijfers = extraCijfers + aanvullen
+        // output.push(aanvullen.charAt(aanvullen))
+    }
+    console.log(extraCijfers)
+
+    
+}
+
+if (output.length > 10 && output.length != 20){
+    console.log(output);
+    console.log("Lengte van de output:", output.length)
+    console.log("Aanvullen tot er weer een tiental uitkomt.")
+}
+
+//------------------------------------------------------
+
+
+
+
