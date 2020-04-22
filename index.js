d05634d9635665431d4f86eba2a28b6cd38fc9df
AlaraEdda <0931871@hr.nl>
2020-04-22 05:13:59 +0200

    Ik heb gemined

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const fetch = require('node-fetch');            //Fetch functie voor node
 const crypto = require('crypto');
 
 //My Code
-// var theCode ="";
-let output = []
-let stringHash = []//Maak array met de echte nummers
+
 
 
 //Functie haalt de laatst aangemaakte block op. 
@@ -21,68 +19,74 @@ async function createHash(){
     const data = await getLatestBlock()                         //Haal de data van de latest make block op. 
     //console.log(data)
 
-    let hashData = data.blockchain.hash                       //Haal hash op vanuit de data. 
+    let hashData = data.blockchain.hash                         //Haal hash op vanuit de data. 
     hashData += data.blockchain.data[0].from
     hashData += data.blockchain.data[0].to
     hashData += data.blockchain.data[0].amount
     hashData += data.blockchain.data[0].timestamp
     hashData += data.blockchain.timestamp
     hashData += data.blockchain.nonce
-    hashData = '000078454c038871fa4d67b0022a30baaf25eaa231f8991b108e2624f052f3f8CMGTMiningCorporationBobPIKAB11548689513858154874778871610312'
+    //console.log("hashdata", hashData)
+    //hashData = '000078454c038871fa4d67b0022a30baaf25eaa231f8991b108e2624f052f3f8CMGTMiningCorporationBobPIKAB11548689513858154874778871610312'
     
     let deGehashteData = await newHash(hashData)
-    //console.log(deGehashteData)
+    console.log("degehastedata", deGehashteData)
 
-    datatoSolve = data.transactions[0].from
+    let datatoSolve = data.transactions[0].from
     datatoSolve += data.transactions[0].to
     datatoSolve += data.transactions[0].amount
     datatoSolve += data.transactions[0].timestamp
     datatoSolve += data.timestamp
-    datatoSolve = 'CMGTMiningCorporationBasBOOTB115487477332611548748101396'
-
+    //datatoSolve = 'CMGTMiningCorporationBasBOOTB115487477332611548748101396'
 
     let i = 0
     let deGesolvdeData = await newHash(deGehashteData + datatoSolve)
-
+    //console.log("Gesolvde: ", deGesolvdeData)
     //Zolang de "deGesolvdeData"-string niet met vier nullen begint... 
     while (!deGesolvdeData.startsWith('0000')){
         //console.log(deGesolvdeData)
         i++
-        deGesolvdeData = await newHash(hashData + datatoSolve + i )
-        console.log(i)
+        //figureoutnonce
+        deGesolvdeData = await newHash(deGehashteData + datatoSolve + i )
+        
     }
-    console.log(deGesolvdeData)
-
-    //checkNonce(i)
+    console.log("Gesolvdedata: ",deGesolvdeData)
+    console.log("nonce: ", i)
+    checkNonce(i)
 }
 
 //Voeg je eigen block toe. 
 function checkNonce(nonce){
+    const body = { nonce: nonce, user: 'Alara Ilhan 0931871' };
     fetch('https://programmeren9.cmgt.hr.nl:8000/api/blockchain', {
         method: 'post',
-        body: {        
-            'nonce': nonce,
-            'user': 'Alara Ilhan 0931871'
-        },
-        headers: {'Content-Type': 'application/json'},
+        body:    JSON.stringify(body),
+        headers: { 'Content-Type': 'application/json' },
     })
-    .then (res => res.json())
+    .then(res => res.json())
     .then(json => console.log(json));
 }
 
 async function newHash(string) {
+    
+    let output = []
+    let stringHash = []                                                        //Maak array met de echte nummers
+    
     //Loop door je string
-    for (let i = 0; i < string.length; i++) {
+    let newString = string.replace(/ /g, "")                                  //Haal spaties in je string weg. 
+
+    for (let i = 0; i < newString.length; i++) {
 
         //Als er een getal zit in je string.
-        if (string.charAt(i)>= '0' && string.charAt(i) <='9'){
-            stringHash.push(parseInt(string.charAt(i)))                     //Het getal dat in mijn string zit, stop ik in mijn stringHash.
+        if (newString.charAt(i)>= '0' && newString.charAt(i) <='9'){
+            stringHash.push(parseInt(newString.charAt(i)))                     //Het getal dat in mijn string zit, stop ik in mijn stringHash.
         }
         else{
             //Text omzetten naar ASC11- Waardes
-            stringHash.push(string.charCodeAt(i));
+            stringHash.push(newString.charCodeAt(i));
         } 
     }
+    //console.log("String", stringHash)
 
     //Zet elke getal los/gescheiden in een array.
     let sCodes = stringHash.join('');
@@ -90,11 +94,13 @@ async function newHash(string) {
     for (let o = 0; o < sCodes.length; o ++) {
         output.push(parseInt(sCodes.charAt(o)))                             //ParseInt maakt er een int van.            
     }
-
+    
     //Voeg cijfers erbij zodat je een array van 10 getallen hebt. 
     for(let b = 0; (output.length %10) !=0 ; b++){                          //Is de lengte van de output deelbaar 10, zolang het antwoord niet 0 is, ga door. 
         output.push(b)
     }
+
+
 
     //console.log("Dit is de orginele output: ", output)
 
@@ -102,64 +108,7 @@ async function newHash(string) {
     return mod10
 
 }
-    //Check hoe groot de array is
-    // if (output.length == 10){
-    //     console.log("Lengte van de output:", output.length)
-    //     console.log("Reeks is een veelvoud van 10")
-    //     //Maar als je dit hebt, heb je dus geen twee rijen?
-    // }
 
-
-    // if (output.length < 10){
-    //     console.log("Lengte van de output:", output.length)
-    //     console.log("Vul reeks aan tot het bestaat uit 10 gehele getallen.")
-
-    //     for(let b = 0; b <10; b++){
-    //         if (output.length<10){
-    //             output.push(b)
-    //         }
-    //     }
-    //     //Maar als je dit hebt, heb je dus geen twee rijen?
-    // }
-
-    // if (output.length > 10 && output.length != 20){
-    //     console.log("Lengte van de output:", output.length)
-    //     console.log("Aanvullen tot er weer een tiental uitkomt.")
-
-    //     for(let b = 0; b <20; b++){
-    //         if (output.length<20){
-    //             output.push(b)
-    //         }
-    //     }
-
-    //     //Splits de array in twee aparte kolommen!
-    //     kolom1 = output.slice(0,10)
-    //     kolom2 = output.slice(10,20)
-    // }
-
-    // //Werkt deze nog effe bij.
-    // if (output.length > 20 && output.length != 30){
-    //     console.log("Lengte van de output:", output.length)
-    //     console.log("Aanvullen tot er weer een tiental uitkomt.")
-
-    //     for(let b = 0; b <30; b++){
-    //         if (output.length<30){
-    //             output.push(b)
-    //         }
-    //     }
-
-    //     //Splits de array in twee aparte kolommen!
-    //     kolom1 = output.slice(0,10)
-    //     kolom2 = output.slice(10,20)
-    //     kolom3 = output.slice(20,30)
-    // }
-    
-
-// console.log("Dit is de nieuwe gehele output:", output)
-// console.log("Dit is kolom1: ",kolom1)
-// console.log("Dit is kolom2: ",kolom2)
-
-//------------------------------------------------------
 
 async function Mod10(kolom1, kolom2, originalkolom, kolom3output = [], cijfer = 0){                                                             //Recursief?
     //Doe Kolom1 + kolom2, creeer zo kolom3!
@@ -188,6 +137,7 @@ function makeHash(kolom3output){
     //console.log("Alles op een rij:", kolom3output.join(''));                           //Tekst naast elkaar.
 
     const hash = crypto.createHash('sha256').update(kolom3output.join('')).digest('hex');
+
     //console.log(hash)
     return hash
 }
---
+++ test.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+let testF = require('./nonse.js');
+testF.createNewHash
+describe('createNewHash', () => {
+    it('gives the correct hash', () =>{
+        return testF.createNewHash('000078454c038871fa4d67b0022a30baaf35eaa231f8991b108e2624f052f3f8CMGTMiningCorporationBobPIKAB1154868951385815487477887161031'){
+            assert.equal(result, '')
+        })
+    })
+})
